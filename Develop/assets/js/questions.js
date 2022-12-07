d2dabe4bc3f973c87930bc4891f22ba9bccf9f5c
Ace <106693166+rickyavila29@users.noreply.github.com>
2022-12-06 19:48:13 -0700

    New repo

---
+++ Develop/assets/js/questions.js
@@ -0,0 +1,35 @@
+// list of all questions, choices, and answers
+var questions = [
+  {
+    title: 'Fill in the blank: if / _____ statement.',
+    choices: ['or', 'and', 'not', 'else'],
+    answer: 'else',
+  },
+  {
+    title: 'Which of the following do not belong:',
+    choices: ['strings', 'booleans', 'alerts', 'numbers'],
+    answer: 'alerts',
+  },
+  {
+    title: 'Arrays in JavaScript can be used to store ____.',
+    choices: [
+      'numbers and strings',
+      'other arrays',
+      'booleans',
+      'all of the above',
+    ],
+    answer: 'all of the above',
+  },
+  {
+    title:
+      'String values must be enclosed within ____ when being assigned to variables.',
+    choices: ['commas', 'curly brackets', 'quotes', 'parentheses'],
+    answer: 'quotes',
+  },
+  {
+    title:
+      'A very useful tool used during development and debugging for printing content to the debugger is:',
+    choices: ['JavaScript', 'terminal / bash', 'for loops', 'console.log'],
+    answer: 'console.log',
+  },
+];
